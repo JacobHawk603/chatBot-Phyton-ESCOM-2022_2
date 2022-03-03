4418d45e13664c35f95d85ea1863d591ba9965dc
AdairNicolas <adairnicolas1@gmail.com>
2022-03-02 20:50:29 -0600

    cambios

--- chatbot.py
+++ chatbot.py
@@ -1,17 +1,22 @@
 #importaciones
+from cgitb import text
+from ctypes import alignment
 import re
 import random
 import json
 
 #Elementos de interfaz grafica
 from tkinter import *
+import tkinter
+from tkinter import font
+from turtle import left
 
 BG_GRAY = "#ABB2B9"
 BG_COLOR = "#17202A"
 TEXT_COLOR = "#EAECEE"
 
-FONT = "Helvetica 14"
-FONT_BOLD = "Helvetica 13 bold"
+FONT = "Helvetica 8"
+FONT_BOLD = "Helvetica 10 bold"
 
 bot_name = "bot"
 
@@ -93,45 +98,45 @@ def check_all_messages(message):
     response(respuestas['articulo28'][0]['respuesta'], ['articulo', 'veintiocho'], required_words=['articulo', 'veintiocho'])
     response(respuestas['articulo29'][0]['respuesta'], ['articulo', 'veintinueve'], required_words=['articulo', 'veintinueve'])
     response(respuestas['articulo30'][0]['respuesta'], ['articulo', 'treinta'], required_words=['articulo', 'treinta'])
-    response(respuestas['articulo31'][0]['respuesta'], ['articulo', 'treinta y uno'], required_words=['articulo', 'treintayuno'])
-    response(respuestas['articulo32'][0]['respuesta'], ['articulo', 'treinta y dos'], required_words=['articulo', 'treintaydos'])
-    response(respuestas['articulo33'][0]['respuesta'], ['articulo', 'treinta y tres'], required_words=['articulo', 'treintaytres'])
-    response(respuestas['articulo34'][0]['respuesta'], ['articulo', 'treinta y cuatro'], required_words=['articulo', 'treintaycuatro'])
-    response(respuestas['articulo35'][0]['respuesta'], ['articulo', 'treinta y cinco'], required_words=['articulo', 'treintaycinco'])
-    response(respuestas['articulo36'][0]['respuesta'], ['articulo', 'treinta y seis'], required_words=['articulo', 'treintayseis'])
-    response(respuestas['articulo37'][0]['respuesta'], ['articulo', 'treinta y siete'], required_words=['articulo', 'treintaysiete'])
-    response(respuestas['articulo38'][0]['respuesta'], ['articulo', 'treinta y ocho'], required_words=['articulo', 'treintayocho'])
-    response(respuestas['articulo39'][0]['respuesta'], ['articulo', 'treinta y nueve'], required_words=['articulo', 'treintaynueve'])
+    response(respuestas['articulo31'][0]['respuesta'], ['articulo', 'treinta y uno'], required_words=['articulo', 'treinta','y', 'uno'])
+    response(respuestas['articulo32'][0]['respuesta'], ['articulo', 'treinta y dos'], required_words=['articulo', 'treinta', 'y', 'dos'])
+    response(respuestas['articulo33'][0]['respuesta'], ['articulo', 'treinta y tres'], required_words=['articulo', 'treinta', 'y', 'tres'])
+    response(respuestas['articulo34'][0]['respuesta'], ['articulo', 'treinta y cuatro'], required_words=['articulo', 'treinta', 'y', 'cuatro'])
+    response(respuestas['articulo35'][0]['respuesta'], ['articulo', 'treinta y cinco'], required_words=['articulo', 'treinta', 'y', 'cinco'])
+    response(respuestas['articulo36'][0]['respuesta'], ['articulo', 'treinta y seis'], required_words=['articulo', 'treinta', 'y', 'seis'])
+    response(respuestas['articulo37'][0]['respuesta'], ['articulo', 'treinta y siete'], required_words=['articulo', 'treinta', 'y', 'siete'])
+    response(respuestas['articulo38'][0]['respuesta'], ['articulo', 'treinta y ocho'], required_words=['articulo', 'treinta', 'y', 'ocho'])
+    response(respuestas['articulo39'][0]['respuesta'], ['articulo', 'treinta y nueve'], required_words=['articulo', 'treinta', 'y', 'nueve'])
     response(respuestas['articulo40'][0]['respuesta'], ['articulo', 'cuarenta'], required_words=['articulo', 'cuarenta'])
-    response(respuestas['articulo41'][0]['respuesta'], ['articulo', 'cuarenta y uno'], required_words=['articulo', 'cuarentayuno'])
-    response(respuestas['articulo42'][0]['respuesta'], ['articulo', 'cuarenta y dos'], required_words=['articulo', 'cuarentaydos'])
-    response(respuestas['articulo43'][0]['respuesta'], ['articulo', 'cuarenta y tres'], required_words=['articulo', 'cuarentaytres'])
-    response(respuestas['articulo44'][0]['respuesta'], ['articulo', 'cuarenta y cuatro'], required_words=['articulo', 'cuarentaycuatro'])
-    response(respuestas['articulo45'][0]['respuesta'], ['articulo', 'cuarenta y cinco'], required_words=['articulo', 'cuarentaycinco'])
-    response(respuestas['articulo46'][0]['respuesta'], ['articulo', 'cuarenta y seis'], required_words=['articulo', 'cuarentayseis'])
-    response(respuestas['articulo47'][0]['respuesta'], ['articulo', 'cuarenta y siete'], required_words=['articulo', 'cuarentaysiete'])
-    response(respuestas['articulo48'][0]['respuesta'], ['articulo', 'cuarenta y ocho'], required_words=['articulo', 'cuarentayocho'])
-    response(respuestas['articulo49'][0]['respuesta'], ['articulo', 'cuarenta y nueve'], required_words=['articulo', 'cuarentaynueve'])
+    response(respuestas['articulo41'][0]['respuesta'], ['articulo', 'cuarenta y uno'], required_words=['articulo', 'cuarenta', 'y', 'uno'])
+    response(respuestas['articulo42'][0]['respuesta'], ['articulo', 'cuarenta y dos'], required_words=['articulo', 'cuarenta', 'y', 'dos'])
+    response(respuestas['articulo43'][0]['respuesta'], ['articulo', 'cuarenta y tres'], required_words=['articulo', 'cuarenta', 'y', 'tres'])
+    response(respuestas['articulo44'][0]['respuesta'], ['articulo', 'cuarenta y cuatro'], required_words=['articulo', 'cuarenta', 'y', 'cuatro'])
+    response(respuestas['articulo45'][0]['respuesta'], ['articulo', 'cuarenta y cinco'], required_words=['articulo', 'cuarenta', 'y', 'cinco'])
+    response(respuestas['articulo46'][0]['respuesta'], ['articulo', 'cuarenta y seis'], required_words=['articulo', 'cuarenta', 'y', 'seis'])
+    response(respuestas['articulo47'][0]['respuesta'], ['articulo', 'cuarenta y siete'], required_words=['articulo', 'cuarenta', 'y', 'siete'])
+    response(respuestas['articulo48'][0]['respuesta'], ['articulo', 'cuarenta y ocho'], required_words=['articulo', 'cuarenta', 'y', 'ocho'])
+    response(respuestas['articulo49'][0]['respuesta'], ['articulo', 'cuarenta y nueve'], required_words=['articulo', 'cuarenta', 'y', 'nueve'])
     response(respuestas['articulo50'][0]['respuesta'], ['articulo', 'cincuenta'], required_words=['articulo', 'cincuenta'])
-    response(respuestas['articulo51'][0]['respuesta'], ['articulo', 'cincuenta y uno'], required_words=['articulo', 'cincuentayuno'])
-    response(respuestas['articulo52'][0]['respuesta'], ['articulo', 'cincuenta y dos'], required_words=['articulo', 'cincuentaydos'])
-    response(respuestas['articulo53'][0]['respuesta'], ['articulo', 'cincuenta y tres'], required_words=['articulo', 'cincuentaytres'])
-    response(respuestas['articulo54'][0]['respuesta'], ['articulo', 'cincuenta  y cuatro'], required_words=['articulo', 'cincuentaycuatro'])
-    response(respuestas['articulo55'][0]['respuesta'], ['articulo', 'cincuenta y cinco'], required_words=['articulo', 'cincuentaycinco'])
-    response(respuestas['articulo56'][0]['respuesta'], ['articulo', 'cincuenta y seis'], required_words=['articulo', 'cincuentayseis'])
-    response(respuestas['articulo57'][0]['respuesta'], ['articulo', 'cincuenta y siete'], required_words=['articulo', 'cincuentaysiete'])
-    response(respuestas['articulo58'][0]['respuesta'], ['articulo', 'cincuenta y ocho'], required_words=['articulo', 'cincuentayocho'])
-    response(respuestas['articulo59'][0]['respuesta'], ['articulo', 'cincuenta y nueve'], required_words=['articulo', 'cincuentaynueve'])
+    response(respuestas['articulo51'][0]['respuesta'], ['articulo', 'cincuenta y uno'], required_words=['articulo', 'cincuenta', 'y', 'uno'])
+    response(respuestas['articulo52'][0]['respuesta'], ['articulo', 'cincuenta y dos'], required_words=['articulo', 'cincuenta', 'y', 'dos'])
+    response(respuestas['articulo53'][0]['respuesta'], ['articulo', 'cincuenta y tres'], required_words=['articulo', 'cincuenta', 'y', 'tres'])
+    response(respuestas['articulo54'][0]['respuesta'], ['articulo', 'cincuenta  y cuatro'], required_words=['articulo', 'cincuenta', 'y', 'cuatro'])
+    response(respuestas['articulo55'][0]['respuesta'], ['articulo', 'cincuenta y cinco'], required_words=['articulo', 'cincuenta', 'y', 'cinco'])
+    response(respuestas['articulo56'][0]['respuesta'], ['articulo', 'cincuenta y seis'], required_words=['articulo', 'cincuenta', 'y', 'seis'])
+    response(respuestas['articulo57'][0]['respuesta'], ['articulo', 'cincuenta y siete'], required_words=['articulo', 'cincuenta', 'y', 'siete'])
+    response(respuestas['articulo58'][0]['respuesta'], ['articulo', 'cincuenta y ocho'], required_words=['articulo', 'cincuenta', 'y', 'ocho'])
+    response(respuestas['articulo59'][0]['respuesta'], ['articulo', 'cincuenta y nueve'], required_words=['articulo', 'cincuenta', 'y', 'nueve'])
     response(respuestas['articulo60'][0]['respuesta'], ['articulo', 'sesenta'], required_words=['articulo', 'sesenta'])
-    response(respuestas['articulo61'][0]['respuesta'], ['articulo', 'sesenta y uno'], required_words=['articulo', 'sesentayuno'])
-    response(respuestas['articulo62'][0]['respuesta'], ['articulo', 'sesenta y dos'], required_words=['articulo', 'sesentaydos'])
-    response(respuestas['articulo63'][0]['respuesta'], ['articulo', 'sesenta y tres'], required_words=['articulo', 'sesentaytres'])
-    response(respuestas['articulo64'][0]['respuesta'], ['articulo', 'sesenta y cuatro'], required_words=['articulo', 'sesentaycuatro'])
-    response(respuestas['articulo65'][0]['respuesta'], ['articulo', 'sesenta y cinco'], required_words=['articulo', 'sesentaycinco'])
-    response(respuestas['articulo66'][0]['respuesta'], ['articulo', 'sesenta y seis'], required_words=['articulo', 'sesentayseis'])
-    response(respuestas['articulo67'][0]['respuesta'], ['articulo', 'sesenta y siete'], required_words=['articulo', 'sesentaysiete'])
-    response(respuestas['articulo68'][0]['respuesta'], ['articulo', 'sesenta y ocho'], required_words=['articulo', 'sesentayocho'])
-    response(respuestas['articulo69'][0]['respuesta'], ['articulo', 'sesenta y nueve'], required_words=['articulo', 'sesentaynueve'])
+    response(respuestas['articulo61'][0]['respuesta'], ['articulo', 'sesenta y uno'], required_words=['articulo', 'sesenta', 'y', 'uno'])
+    response(respuestas['articulo62'][0]['respuesta'], ['articulo', 'sesenta y dos'], required_words=['articulo', 'sesenta', 'y', 'dos'])
+    response(respuestas['articulo63'][0]['respuesta'], ['articulo', 'sesenta y tres'], required_words=['articulo', 'sesenta', 'y', 'tres'])
+    response(respuestas['articulo64'][0]['respuesta'], ['articulo', 'sesenta y cuatro'], required_words=['articulo', 'sesenta', 'y', 'cuatro'])
+    response(respuestas['articulo65'][0]['respuesta'], ['articulo', 'sesenta y cinco'], required_words=['articulo', 'sesenta', 'y', 'cinco'])
+    response(respuestas['articulo66'][0]['respuesta'], ['articulo', 'sesenta y seis'], required_words=['articulo', 'sesenta', 'y', 'seis'])
+    response(respuestas['articulo67'][0]['respuesta'], ['articulo', 'sesenta y siete'], required_words=['articulo', 'sesenta', 'y', 'siete'])
+    response(respuestas['articulo68'][0]['respuesta'], ['articulo', 'sesenta y ocho'], required_words=['articulo', 'sesenta', 'y', 'ocho'])
+    response(respuestas['articulo69'][0]['respuesta'], ['articulo', 'sesenta y nueve'], required_words=['articulo', 'sesenta', 'y', 'nueve'])
     response(respuestas['articulo70'][0]['respuesta'], ['articulo', 'setenta'], required_words=['articulo', 'setenta'])
 
     best_match = max(highest_prob, key=highest_prob.get)
@@ -156,12 +161,12 @@ while True:
             
         def _setup_main_window(self):
             self.window.title("Chat")
-            self.window.resizable(width=False, height=False)
-            self.window.configure(width=470, height=550, bg=BG_COLOR)
+            self.window.resizable(width=True, height=True)
+            self.window.configure(width=1000, height=550, bg=BG_COLOR)
             
             # head label
             head_label = Label(self.window, bg=BG_COLOR, fg=TEXT_COLOR,
-                            text="Welcome", font=FONT_BOLD, pady=10)
+                            text="Bienvenido", font=FONT_BOLD, pady=10)
             head_label.place(relwidth=1)
             
             # tiny divider
@@ -169,8 +174,8 @@ while True:
             line.place(relwidth=1, rely=0.07, relheight=0.012)
             
             # text widget
-            self.text_widget = Text(self.window, width=20, height=2, bg=BG_COLOR, fg=TEXT_COLOR,
-                                    font=FONT, padx=5, pady=5)
+            self.text_widget = Text(self.window, width=20, height=1, bg=BG_COLOR, fg=TEXT_COLOR,
+                                    font=FONT, padx=3, pady=5)
             self.text_widget.place(relheight=0.745, relwidth=1, rely=0.08)
             self.text_widget.configure(cursor="arrow", state=DISABLED)
             
@@ -180,8 +185,8 @@ while True:
             scrollbar.configure(command=self.text_widget.yview)
             
             # bottom label
-            bottom_label = Label(self.window, bg=BG_GRAY, height=80)
-            bottom_label.place(relwidth=1, rely=0.825)
+            bottom_label = Label(self.window, bg=BG_GRAY, height=30)
+            bottom_label.place(relwidth=1, rely=0.925)
             
             # message entry box
             self.msg_entry = Entry(bottom_label, bg="#2C3E50", fg=TEXT_COLOR, font=FONT)
@@ -190,20 +195,20 @@ while True:
             self.msg_entry.bind("<Return>", self._on_enter_pressed)
             
             # send button
-            send_button = Button(bottom_label, text="Send", font=FONT_BOLD, width=20, bg=BG_GRAY,
+            send_button = Button(bottom_label, text="Enviar", font=FONT_BOLD, width=20, bg=BG_GRAY,
                                 command=lambda: self._on_enter_pressed(None))
             send_button.place(relx=0.77, rely=0.008, relheight=0.06, relwidth=0.22)
         
         def _on_enter_pressed(self, event):
             msg = self.msg_entry.get()
-            self._insert_message(msg, "You")
+            self._insert_message(msg, "Usuario")
             
         def _insert_message(self, msg, sender):
             if not msg:
                 return
             
             self.msg_entry.delete(0, END)
-            msg1 = f"{sender}: {msg}\n\n"
+            msg1 = f" {sender}: {msg}\n\n".format(font)
             self.text_widget.configure(state=NORMAL)
             self.text_widget.insert(END, msg1)
             self.text_widget.configure(state=DISABLED)
